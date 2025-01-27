50f023335f62431473e289374d8c33d8c1df6100
«Im-Abyss» <enty3811@gmail.com>
2025-01-27 18:30:44 +0300

    Hotfix

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, LabeledPrice
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 from aiogram.exceptions import TelegramBadRequest
 
